dcf5171b5a3f7582322d50fbefc7298f532fc106
Verena Blaschke <verena.blaschke@gmx.de>
2020-12-06 23:50:55 +0100

    parameterized predict

--- predict.py
+++ predict.py
@@ -7,34 +7,40 @@ from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
 from ngram_lime.lime.lime_text import LimeTextExplainer
 import re
 import sys
+import argparse
+import pickle
 
 
-if len(sys.argv) == 1:
-    print("Expected one argument: 'dialect' or 'tweet'")
-    sys.exit()
-if sys.argv[1] == 'dialect':
+parser = argparse.ArgumentParser()
+parser.add_argument('type', type=str,
+                    help="'dialects' or 'tweets'")
+parser.add_argument('model')
+parser.add_argument('--start', dest='start_idx', default=0, type=int)
+parser.add_argument('--stop', dest='stop_idx', default=-1, type=int)
+parser.add_argument('--load', dest='load_model', type=bool, default=False)
+args = parser.parse_args()
+
+
+if args.type == 'dialects':
     DIALECTS = True
-elif sys.argv[1] == 'tweet':
+elif args.type == 'tweets':
     DIALECTS = False
 else:
-    print("Expected one argument: 'dialect' or 'tweet'")
+    print("Expected 'dialects' or 'tweets'")
+    sys.exit()
+LINEAR_SVC = DIALECTS
+
+if args.load_model and not args.model:
+    print("You need to provide a path to the model to load it")
     sys.exit()
 
 
 np.random.seed(42)
+SAVE_LOC = args.model
 
-
-if DIALECTS:
-    FILE = 'data/phon_cleaned.tsv'
-    # FILE = 'gdrive/My Drive/colab_projects/phon_cleaned.tsv'
-else:
-    FILE = 'data/tweets_cleaned.tsv'
-    # FILE = 'gdrive/My Drive/colab_projects/tweets_cleaned.tsv'    
 
 # U0329, U030D are the combining lines for marking syllabic consonants
 char_pattern = re.compile(r'(\w[\u0329\u030D]*|\.\w)', re.UNICODE | re.IGNORECASE)
-
-LINEAR_SVC = DIALECTS
 
 
 GET_NGRAMS = True
@@ -56,6 +62,7 @@ GET_NGRAMS = True
     
 # def utterance2ngrams(utterance, word_ns=[1, 2], char_ns=[1, 2, 3, 4, 5], verbose=False):
 def utterance2ngrams(utterance, word_ns=[1, 2], char_ns=[2, 3, 4, 5], verbose=False):
+    utterance = utterance.strip()
     if DIALECTS:
         words = utterance.split()
     else:
@@ -159,7 +166,7 @@ def preprocess_and_vectorize(utterance, vectorizer):
     # return vectorizer.transform([preprocess(utterance)])
 
 
-def train(train_x, train_y, linear_svc=LINEAR_SVC, class_weight=None):
+def train(train_x, train_y, linear_svc, class_weight=None):
     if linear_svc:
         model = svm.LinearSVC(C=1.0, class_weight=class_weight)
     else:
@@ -173,7 +180,7 @@ def predict(model, test_x):
     return model.predict(test_x)
 
 
-def predict_instance(model, utterance, label_encoder, vectorizer, linear_svc=LINEAR_SVC):
+def predict_instance(model, utterance, label_encoder, vectorizer, linear_svc):
     # x = vectorizer.transform([preprocess(utterance)])
     x = vectorizer.transform([utterance])
     pred = model.predict(x)
@@ -199,7 +206,7 @@ def predict_instance(model, utterance, label_encoder, vectorizer, linear_svc=LIN
 #     return probs
 
 
-def predict_proba2(model, data, vectorizer, n_labels=4, linear_svc=LINEAR_SVC):
+def predict_proba2(model, data, vectorizer, linear_svc, n_labels=4):
     global GET_NGRAMS
     # print('data', data)
     probs = np.zeros((len(data), n_labels))
@@ -270,24 +277,73 @@ def instances_far_from_decision_boundary(model, label_encoder, train_x,
         print('\n\n')
 
 
-label_col = 0 if DIALECTS else 1
-data_col = 4 if DIALECTS else 2
-train_x, test_x, train_x_raw, test_x_raw, train_y, test_y, label_encoder, vectorizer = parse_file(FILE, label_col=label_col, data_col=data_col)
-# model = train(train_x, train_y)
-# model = train(train_x, train_y, class_weight='balanced')
-class_weight = None if DIALECTS else {0: 1, 1: 2}
-model = train(train_x, train_y, class_weight=class_weight)
-pred = predict(model, test_x)
-score(pred, test_y)
+
+
+if args.load_model:
+    print("Loading the model.")
+    test_x_raw = []
+    with open(SAVE_LOC + '/vectorizer.pickle', 'r', encoding='utf8') as f:
+        for line in f:
+            test_x_raw.append(line.strip())
+    test_y = np.load(SAVE_LOC + '/test_y.npy')
+    assert len(test_x_raw) == len(test_y)
+    label_encoder = LabelEncoder()
+    label_encoder.classes_ = np.load(SAVE_LOC + '/label_encoder_classes.npy')
+    with open(SAVE_LOC + '/vectorizer.pickle', 'rb') as f:
+        vectorizer = pickle.load(f)
+    with open(SAVE_LOC + '/classifier.pickle', 'rb') as f:
+        classifier = pickle.load(f)
+    print("Done.")
+else:
+    if DIALECTS:
+        FILE = 'data/phon_cleaned.tsv'
+        # FILE = 'gdrive/My Drive/colab_projects/phon_cleaned.tsv'
+    else:
+        FILE = 'data/tweets_cleaned.tsv'
+        # FILE = 'gdrive/My Drive/colab_projects/tweets_cleaned.tsv'    
+
+    print("Preparing the data")
+    label_col = 0 if DIALECTS else 1
+    data_col = 4 if DIALECTS else 2
+    train_x, test_x, train_x_raw, test_x_raw, train_y, test_y, label_encoder, vectorizer = parse_file(FILE, label_col=label_col, data_col=data_col)
+    print("Training the model")
+    class_weight = None if DIALECTS else {0: 1, 1: 2}
+    classifier = train(train_x, train_y, linear_svc=LINEAR_SVC, class_weight=class_weight)
+    print("Scoring the model")
+    pred = predict(classifier, test_x)
+    score(pred, test_y)
+
+    print("Saving the model.")
+    with open(SAVE_LOC + '/vectorizer.pickle', 'w', encoding='utf8') as f:
+        for x in test_x_raw:
+            f.write(x + '\n')
+    np.save(SAVE_LOC + '/test_y.npy', test_y)
+    np.save(SAVE_LOC + '/label_encoder_classes.npy', label_encoder.classes_)
+    with open(SAVE_LOC + '/vectorizer.pickle', 'wb') as f:
+        pickle.dump(vectorizer, f)
+    with open(SAVE_LOC + '/classifier.pickle', 'wb') as f:
+        pickle.dump(classifier, f)
+
+
 
 # # Roughly the baseline in the An Annotated Corpus... paper
 # train_x_bs, test_x_bs, train_x_raw_bs, test_x_raw_bs, train_y_bs, test_y_bs, label_encoder_bs, vectorizer_bs = parse_file(FILE, label_col=1, data_col=2,
 #     analyzer=lambda x: utterance2ngrams(x, word_ns=[1], char_ns=[], verbose=False))
-# model_bs = train(train_x_bs, train_y_bs, class_weight={0: 1, 1: 2})
-# pred_bs = predict(model_bs, test_x_bs)
+# classifier_bs = train(train_x_bs, train_y_bs, class_weight={0: 1, 1: 2})
+# pred_bs = predict(classifier_bs, test_x_bs)
 # score(pred_bs, test_y_bs)
-# support_vectors(model, label_encoder, train_x, train_x_raw)
-# instances_far_from_decision_boundary(model, label_encoder, train_x, train_x_raw)
+# support_vectors(classifier, label_encoder, train_x, train_x_raw)
+# instances_far_from_decision_boundary(classifier, label_encoder, train_x, train_x_raw)
+
+
+def save_to_file(filename, results_0, results_1, results_2, results_3):
+    res = [results_0, results_1]
+    if DIALECTS:
+        res += [results_2, results_3]
+    for i, r in enumerate(res):
+        with open(filename.format(i), 'a', encoding='utf8') as f:
+            for feature, score in r:
+                f.write('{}\t{:.10f}\n'.format(feature, score))
 
 
 labels = [0, 1, 2, 3] if DIALECTS else [0, 1]
@@ -298,16 +354,16 @@ explainer = LimeTextExplainer(class_names=label_encoder.inverse_transform(labels
                               utterance2ngrams=utterance2ngrams,
                               ngram_lvl=True,
                               recalculate_ngrams=False)
-labels = [0, 1, 2, 3] if DIALECTS else [0, 1]
 # lime_results = {0: dict(), 1: dict(), 2: dict(), 3: dict()}
 results_0 = []
 results_1 = []
 results_2 = []
 results_3 = []
-for i, utterance in enumerate(test_x_raw):
+filename = 'results/results_dialects_{}.txt' if DIALECTS else 'results/results_tweets_{}.txt'
+for i, utterance in enumerate(test_x_raw[args.start_idx:args.stop_idx]):
     label = test_y[i]
     exp = explainer.explain_instance(utterance,
-                                     lambda z: predict_proba2(model, z, vectorizer, n_labels=len(labels)),
+                                     lambda z: predict_proba2(classifier, z, vectorizer, LINEAR_SVC, n_labels=len(labels)),
                                      num_features=10,
                                      labels=labels,
                                      # labels=interesting_labels
@@ -322,7 +378,7 @@ for i, utterance in enumerate(test_x_raw):
 
     if i % 50 == 0:
         interesting_labels = [label]
-        pred = predict_instance(model, utterance, label_encoder, vectorizer)
+        pred = predict_instance(classifier, utterance, label_encoder, vectorizer, LINEAR_SVC)
         print(i)
         print('"' + utterance + '""')
         print('ACTUAL', label_encoder.inverse_transform([label])[0])
@@ -332,15 +388,11 @@ for i, utterance in enumerate(test_x_raw):
         # for l in interesting_labels:
         #     exp.show_in_notebook(text=utterance, labels=(l,))
         print('\n')
+        save_to_file(filename, results_0, results_1, results_2, results_3)
+        results_0 = []
+        results_1 = []
+        results_2 = []
+        results_3 = []
     i += 1
     
-
-res = [results_0, results_1]
-if DIALECTS:
-    res += [results_2, results_3]
-
-filename = 'results_dialects_{}.txt' if DIALECTS else 'results_tweets_{}.txt'
-for i, res in enumerate([res]):
-    with open(filename.format(i), 'w', encoding='utf8') as f:
-        for feature, score in res:
-            f.write('{}\t{:.10f}\n'.format(feature, score))
+save_to_file(filename, results_0, results_1, results_2, results_3)
