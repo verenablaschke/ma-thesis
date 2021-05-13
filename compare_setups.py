6cf193124be003097965f7db444926608cb275f7
Verena Blaschke <verena.blaschke@gmx.de>
2021-05-13 17:06:15 +0200

    aggregate set-up comparison

--- compare_setups.py
+++ compare_setups.py
@@ -1,19 +1,61 @@
 import argparse
 import os
+import numpy as np
+from pathlib import Path
+
+
+def get_fold_scores(fold):
+    with open(fold + '/setup-scores.tsv', 'w+', encoding='utf8') as f_out:
+        f_out.write('SET-UP\tF1\tACCURACY\n')
+        for _, _, files in os.walk(fold):
+            for file in files:
+                if 'log' in file:
+                    with open(fold + '/' + file, encoding='utf8') as f_in:
+                        for line in f_in:
+                            if line.startswith('Accuracy'):
+                                acc = line.strip().split('\t')[1]
+                            if line.startswith('F1'):
+                                f1 = line.strip().split('\t')[1]
+                    f_out.write('{}\t{}\t{}\n'.format(file, f1, acc))
+
+
+def get_aggregate_scores(folds):
+    f1_scores = {}
+    acc_scores = {}
+    for fold in folds:
+        with open(fold + '/setup-scores.tsv', encoding='utf8') as f:
+            next(f)
+            for line in f:
+                cells = line.strip().split('\t')
+                run, f1, acc = cells[0], float(cells[1]), float(cells[2])
+                try:
+                    f1_scores[run].append(f1)
+                    acc_scores[run].append(acc)
+                except KeyError:
+                    f1_scores[run] = [f1]
+                    acc_scores[run] = [acc]
+    f1_list = [(run, np.mean(f1), len(f1)) for run, f1 in f1_scores.items()]
+    f1_list.sort(key=lambda x: x[1], reverse=True)
+    with open(Path(folds[0]).parent.absolute() / 'setup-scores-aggregate.tsv',
+              'w+', encoding='utf8') as f:
+        f.write('RUN\tN\tF1\tACCURACY\n')
+        for (run, f1_mean, n) in f1_list:
+            if n < len(folds) - 1:
+                continue
+            if run in ('log.txt', 'log_acc.tsv', 'log_f1.tsv'):
+                continue
+            f.write('{}\t{}\t{:.3f}\t{:.3f}\n'.format(
+                run, n, f1_mean, np.mean(acc_scores[run])))
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument('fold')
+parser.add_argument('folds', nargs='+')
+parser.add_argument('--a', dest='aggregate', default=False,
+                    action='store_true')
 args = parser.parse_args()
 
-with open(args.fold + '/setup-scores.tsv', 'w+', encoding='utf8') as f_out:
-    f_out.write('SET-UP\tF1\tACCURACY\n')
-    for _, _, files in os.walk(args.fold):
-        for file in files:
-            if 'log' in file:
-                with open(args.fold + '/' + file, 'r', encoding='utf8') as f_in:
-                    for line in f_in:
-                        if line.startswith('Accuracy'):
-                            acc = line.strip().split('\t')[1]
-                        if line.startswith('F1'):
-                            f1 = line.strip().split('\t')[1]
-                f_out.write('{}\t{}\t{}\n'.format(file, f1, acc))
+if args.aggregate:
+    get_aggregate_scores(args.folds)
+else:
+    for fold in args.folds:
+        get_fold_scores(fold)
--- dialects_predict.sh
+++ dialects_predict.sh
@@ -3,13 +3,13 @@
 # In the case of line ending problems (Windows vs. UNIX encodings), run:
 # sed -i 's/\r$//' dialects.sh
 
-python3 extract_features.py dialects models/dialects --word "[1,2]" --char "[1,2,3,4,5]"
-python3 feature_correlation.py models/dialects
-python3 prepare_folds.py models/dialects 10
+taskset -c 0-19 python3 extract_features.py dialects models/dialects-may --word "[1,2]" --char "[1,2,3,4,5]"
+taskset -c 0-19 python3 feature_correlation.py models/dialects-may
+taskset -c 0-19 python3 prepare_folds.py models/dialects-may 10
 
 for i in $(seq 0 9); do
     echo "Setting up fold $i"
     screen -dmS dialects$i
-    screen -S dialects$i -X stuff "python3 predict_fold.py models/dialects dialects $i
+    screen -S dialects$i -X stuff "taskset -c 0-19 python3 predict_fold.py models/dialects-may dialects $i --z 1000 --limefeat 100
 "
 done
